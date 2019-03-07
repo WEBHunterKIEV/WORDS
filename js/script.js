9af5787124475ac7b80bb157274d89d14f5b8f40
Hunter1 <info@web-hunters.com>
2019-03-07 11:40:20 +0200

    7031140

--- js/script.js
+++ js/script.js
@@ -20,16 +20,27 @@ $(document).ready(function () {
 
 	$('.keyboard').click(function(e) {
 
+		$('.keyboard').fadeTo("slow", 0.1)
+		$('.confirm').addClass('confirm_show')
+
+		word = word.split(''); 
 		
+		word[rand] = e.target.innerText;
+
+		//word[rand].css('color','red')
+
+		word = word.join('');
+		$('#word').text(word)
 
 		//--------------------------------------
 
 		if (e.target.innerText == randLetter) {
-			alert('Yes')
+			//alert('Yes')
 		} else {
-			alert('no')
+			//alert('no')
 		}
 
+		console.log(word)
 		//console.log(e.target.innerText + " / " + randLetter)
 	})
 
