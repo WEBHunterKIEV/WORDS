11035ed414f046397e21cfed8aacd045b230d3d3
Hunter1 <info@web-hunters.com>
2019-03-29 21:30:19 +0200

    29-03-2019

--- js/script.js
+++ js/script.js
@@ -2,14 +2,48 @@
 $(document).ready(function () {
 
   var base = [
-		{"name":"КОТ","img":"images/cat.png"},
+		{"name":"КІТ","img":"images/cat.png"},
 		{"name":"СЛОН","img":"images/elef.png"},
-		{"name":"ЗАЯЦ","img":"images/banny.png"},
-		{"name":"МЕДВЕДЬ","img":"images/bear.png"},
+		{"name":"ЗАЄЦЬ","img":"images/banny.png"},
+		{"name":"ВЕДМЕДЬ","img":"images/bear.png"},
 		{"name":"БЕГЕМОТ","img":"images/hippo.png"},
 		{"name":"ТИГР","img":"images/tiger.png"},
-		{"name":"ВОЛК","img":"images/woolf.png"}
+		{"name":"ВОВК","img":"images/woolf.png"},
+		{"name":"ЧАРІВНИЦЯ","img":"images/magikgirl.png"},
+		{"name":"сніжинка","img":"images/snow.png"},
+		{"name":"завірюха","img":"images/zaviruh.png"},
+		{"name":"бурульки","img":"images/burulki.png"},
+		{"name":"хмари","img":"images/hmari.jpg"},
+		{"name":"дощик","img":"images/rain.jpg"},
+		{"name":"гілочки","img":"images/gilochki.png"},
+		{"name":"ковзани","img":"images/kovzani.webp"},
+		{"name":"ялинка","img":"images/yalinka.png"},
+		{"name":"кульки","img":"images/kulki.png"},
+		{"name":"квіти","img":"images/flowers.png"},
+		{"name":"засмучений","img":"images/zasmuch.jpg"},
+		{"name":"камінець","img":"images/kaminetc.jpg"},
+		{"name":"дивитись","img":"images/divitis.png"},
+		{"name":"сонечко","img":"images/sun.png"},
+		{"name":"бджілка","img":"images/bee.png"},
+
+		{"name":"АЗБУКА","img":"images/azbuka.jpg"},
+		{"name":"СЛОВНИК","img":"images/slovnik.jpg"},
+		{"name":"УЧЕНЬ","img":"images/uchen.jpg"},
+		{"name":"НАВЧАННЯ","img":"images/navchanna.jpg"},
+		{"name":"МОВА","img":"images/mova.jpg"},
+		{"name":"АЛФАВІТ","img":"images/azbuka.jpg"},
+		{"name":"ВПРАВА","img":"images/vprava.jpg"},
+		{"name":"ЗВУК","img":"images/zvuk.jpg"},
+		{"name":"КНИГА","img":"images/kniga.jpg"},
+		{"name":"СЛОВО","img":"images/word.jpg"}
 	]
+
+	//--------------------------------- apper case
+
+  for (i=0; i<base.length; i++) {
+		base[i].name = base[i].name.toUpperCase()
+		console.log(base[i].name)
+	}
 
 	//--------------------------------- init history log
 
@@ -22,12 +56,21 @@ $(document).ready(function () {
 	
 	//--------------------------------- head text
 
-	var totalCounter = window.localStorage.totalCounter;
-	if (totalCounter == undefined) totalCounter = 0;
-	var correctCounter = window.localStorage.correctCounter;
-	if (correctCounter == undefined) correctCounter = 0;
-	var incorrectCounter = window.localStorage.incorrectCounter;
-	if (incorrectCounter == undefined) incorrectCounter = 0;
+	totalCounter = window.localStorage.totalCounter;
+	if (totalCounter == undefined) {
+		totalCounter = 0;
+		window.localStorage.totalCounter = totalCounter;
+	}
+	correctCounter = window.localStorage.correctCounter;
+	if (correctCounter == undefined) {
+		correctCounter = 0;
+		window.localStorage.correctCounter = correctCounter;
+	}
+	incorrectCounter = window.localStorage.incorrectCounter;
+	if (incorrectCounter == undefined) {
+		incorrectCounter = 0;
+		window.localStorage.incorrectCounter = incorrectCounter;
+	}
 
 		$('#blue_span').text('СЛОВ ЗА СЕГОДНЯ: ' + totalCounter);
 		$('#green_span').text('ПРАВИЛЬНО: ' + correctCounter);
@@ -74,15 +117,8 @@ $(document).ready(function () {
 		
 		word[rand] = e.target.innerText;
 
-		//word[rand].css('color','red')
-
 		word = word.join('');
-		$('#word').text(word)
-
-
-
-
-		
+		$('#word').text(word)	
 	})
 
 		//-------------------------------------
@@ -102,7 +138,15 @@ $(document).ready(function () {
 			} else {
 				if (word[rand] == randLetter) {
 					$(".answer_correct").addClass('flex_show');
+					totalCounter++;
+					window.localStorage.totalCounter = totalCounter;
+					correctCounter++;
+					window.localStorage.correctCounter = correctCounter;
 					} else {
+						totalCounter++;
+						window.localStorage.totalCounter = totalCounter;
+						incorrectCounter++;
+						window.localStorage.incorrectCounter = incorrectCounter;
 					$(".answer_incorrect").addClass('flex_show');
 					$("#right_answer").text(base[randTask].name);
 
@@ -118,6 +162,8 @@ $(document).ready(function () {
 					var answerNom = 'answer' + window.localStorage.counter;
 
 					window.localStorage[answerNom] = d + "  >>  " + $('#word').text() + " // " + base[randTask].name;
+
+
 				
 
 					}
@@ -138,7 +184,11 @@ $(document).ready(function () {
 			$('#history').toggleClass('history_open');
 		})
 
-	
+
+		//--------------------------------- toggle history class
+
+
+
 
 	
 })
